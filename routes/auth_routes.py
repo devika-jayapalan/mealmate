773c55acc67d9e54cf6d11d7eab775e77b4603d7
Needlebrook <celiavictor101@gmail.com>
2025-07-08 18:41:26 +0530

    phone -> email

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -7,16 +7,16 @@ auth_bp = Blueprint('auth', __name__)
 def login():
     if request.method == 'POST':
         name = request.form.get('name')
-        phone = request.form.get('phone')
+        email = request.form.get('email')
         mongo = current_app.mongo
 
         # Check if user exists
-        user = mongo.db.users.find_one({"phone": phone})
+        user = mongo.db.users.find_one({"email": "email"})
         if not user:
             # Create new user
             user_id = mongo.db.users.insert_one({
                 "name": name,
-                "phone": phone
+                "email": email,
             }).inserted_id
         else:
             user_id = user['_id']

a00bd4e29dc167a1d7835c8c1ddf4b433bb84c44
Needlebrook <celiavictor101@gmail.com>
2025-07-08 20:40:34 +0530

    Recipes

--- routes/ingredient_routes.py
+++ routes/ingredient_routes.py
@@ -6,13 +6,13 @@ ingredient_bp = Blueprint('ingredient', __name__)
 @ingredient_bp.route('/ingredients', methods=['GET', 'POST'])
 def ingredients():
     mongo = current_app.mongo
-    user_id = session.get('user_id')  # may be None
+    user_id = session.get('user_id')
 
     if request.method == 'POST':
         raw_input = request.form.get('ingredients')
         ingredients = [i.strip() for i in raw_input.split('\n') if i.strip()]
 
-        # Only save to DB if user is logged in
+        #if user is logged in
         if user_id:
             for item in ingredients:
                 mongo.db.ingredients.insert_one({
